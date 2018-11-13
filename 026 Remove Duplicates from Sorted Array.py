65c041a0abc26bb13fccf0c83efa83e3823a99b6
Sue <ssica_789@163.com>
2018-11-13 18:28:50 +0800

    Leetcode 026, haven't done yet

---
+++ 026 Remove Duplicates from Sorted Array.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 17:35:54 2018
+
+@author: Sue
+
+026 Remove Duplicates from Sorted Array
+"""
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return len(nums)
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while 
+        
